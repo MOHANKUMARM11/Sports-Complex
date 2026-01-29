b672a328f0202f624e7dfb03eae4f53ccf4e501d
Pranesh-I <ipranesh046@gmail.com>
2026-01-29 11:12:40 +0530

    height modified

--- backend/main.py
+++ backend/main.py
@@ -2,4 +2,9 @@ from fastapi import FastAPI
 from api.height import router as height_router
 
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "Backend is running"}
+
 app.include_router(height_router)
